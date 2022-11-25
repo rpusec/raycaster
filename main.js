844c8c467c11a5835117da21195d1c45d6d6848f
Roman Pusec <roman.pusec@gmail.com>
2022-11-25 22:39:03 +0100

    Revert "created src folder"
    This reverts commit 9ed3b075d6cd26f10a08ef7ecd2ed40f7eaa21f7.

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile(__dirname + '/renderer/index.html')
+  mainWindow.loadFile('renderer/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
